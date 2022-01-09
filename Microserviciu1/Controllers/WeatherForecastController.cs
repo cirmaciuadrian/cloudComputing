cb191196124874a0d96875af546cb249ca984207
Adrian Cirmaciu <adrian.cirmaciu@itom.ro>
2022-01-09 17:26:36 +0200

    comunicare intre proiecutl de web si microservicii

--- Microserviciu1/Controllers/WeatherForecastController.cs
+++ Microserviciu1/Controllers/WeatherForecastController.cs
@@ -24,16 +24,19 @@ namespace Microserviciu1.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<LibraryData.Classes.Class> Get(string orice)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+
+           return LibraryData.DataAccess.Repository.GeneralRepository.GetClasses().ToList();
+
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
         }
     }
 }
--- SmartCloud/Controllers/HomeController.cs
+++ SmartCloud/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SmartCloud.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SmartCloud.Controllers
@@ -12,14 +16,44 @@ namespace SmartCloud.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static HttpClient httpClient = new HttpClient();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var url = "https://localhost:49163/weatherforecast";
+            var data = new
+            {
+                orice = "orice"
+            };
+            var httpResponse = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, url)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")
+            });
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var response = await httpResponse.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(response);
+                var jModel_Offers = json.Value<JObject>("Result");
+                var result = jModel_Offers.ToString();
+            }
+            //    datalist = Newtonsoft.Json.JsonConvert.DeserializeObject<TapWaterGetOfferWrapper>(result);
+
+
+
+            //    if (datalist.offer.Count > 0)
+            //    {
+            //        foreach (var item in datalist.offer)
+            //        {
+            //            listaFacturiCuOferte = listaFacturiCuOferte + "," + item.product.invoiceDiscounting.invoiceReferenceId;
+            //        }
+            //        listaFacturiCuOferte = listaFacturiCuOferte.Substring(1);
+            //    }
+            //}
             return View();
         }
 
